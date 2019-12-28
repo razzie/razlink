f8b88c2bab83c40280faee15efa40c19175528ce
Gábor Görzsöny <gabor@gorzsony.com>
2019-12-28 18:07:32 +0100

    Changed default parameters for better testability

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	hostname := flag.String("hostname", "link.gorzsony.com", "Hostname")
-	port := flag.Int("port", 8081, "Port")
+	hostname := flag.String("hostname", "localhost:8080", "Hostname")
+	port := flag.Int("port", 8080, "Port")
 	redisAddr := flag.String("redis-addr", "localhost:6379", "Redis hostname:port")
 	redisPw := flag.String("redis-pw", "", "Redis password")
 	redisDb := flag.Int("redis-db", 0, "Redis database (0-15)")
