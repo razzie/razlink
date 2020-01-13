a1b8124c25d96dd56e7f1d62737599325a356d17
Gábor Görzsöny <gabor@gorzsony.com>
2020-01-13 04:18:02 +0100

    Added CLI mode

---
+++ cli.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// CLI ...
+type CLI struct {
+	cmds map[string]func(args []string)
+}
+
+func newCLI(db *DB) *CLI {
+	cli := &CLI{
+		cmds: make(map[string]func(args []string)),
+	}
+
+	cli.cmds["help"] = func(args []string) {
+		for cmd := range cli.cmds {
+			fmt.Println(cmd)
+		}
+	}
+
+	cli.cmds["links"] = func(args []string) {
+		if len(args) != 1 {
+			fmt.Println("usage: links <pattern>")
+			return
+		}
+
+		pattern := args[0]
+		keys, err := db.client.Keys(pattern).Result()
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		for _, key := range keys {
+			if strings.HasSuffix(key, "-log") {
+				continue
+			}
+
+			e, err := db.GetEntry(key)
+			if err != nil {
+				continue
+			}
+
+			fmt.Println(e.ID, "-", e.URL)
+		}
+	}
+
+	cli.cmds["add"] = func(args []string) {
+		if len(args) != 2 {
+			fmt.Println("usage: add <URL> <password>")
+			return
+		}
+
+		url := args[0]
+		pw := args[1]
+		method, err := GetServeMethodForURL(context.Background(), url)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		e, err := db.InsertEntry(url, pw, method)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		fmt.Println("added:", e.ID)
+	}
+
+	cli.cmds["add-permanent"] = func(args []string) {
+		if len(args) != 3 {
+			fmt.Println("usage: add-permament <ID> <URL> <password>")
+			return
+		}
+
+		ID := args[0]
+		url := args[1]
+		pw := args[2]
+		method, err := GetServeMethodForURL(context.Background(), url)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		e, err := db.InsertPermanentEntry(ID, url, pw, method)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		fmt.Println("added:", e.ID)
+	}
+
+	cli.cmds["delete"] = func(args []string) {
+		if len(args) != 1 {
+			fmt.Println("usage: delete <ID>")
+			return
+		}
+
+		ID := args[0]
+		err := db.DeleteEntry(ID)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		fmt.Println("deleted:", ID)
+	}
+
+	cli.cmds["logs"] = func(args []string) {
+		if len(args) != 3 {
+			fmt.Println("usage: logs <ID> <first> <last>")
+			return
+		}
+
+		ID := args[0]
+		first, _ := strconv.Atoi(args[1])
+		last, _ := strconv.Atoi(args[2])
+		logs, err := db.GetLogs(ID, first, last)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		for _, log := range logs {
+			fmt.Println(log.String())
+		}
+	}
+
+	cli.cmds["clear-logs"] = func(args []string) {
+		if len(args) != 1 {
+			fmt.Println("usage: clear-logs <ID>")
+			return
+		}
+
+		ID := args[0]
+		err := db.DeleteLogs(ID)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+
+		fmt.Println("logs cleared:", ID)
+	}
+
+	cli.cmds["exit"] = func(args []string) {
+		os.Exit(0)
+	}
+
+	return cli
+}
+
+func (cli *CLI) handleCommand(cmd string) {
+	args := strings.Fields(cmd)
+	if len(args) == 0 {
+		return
+	}
+
+	fn, ok := cli.cmds[args[0]]
+	if !ok {
+		fmt.Println("unknown command:", cmd)
+		return
+	}
+
+	fn(args[1:])
+}
+
+// Run ...
+func (cli *CLI) Run() {
+	fmt.Println("awesome razlink command line")
+	fmt.Println("----------------------------")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("-> ")
+		cmd, _ := reader.ReadString('\n')
+		cmd = strings.TrimSuffix(cmd, "\n")
+
+		cli.handleCommand(cmd)
+	}
+}
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	redisAddr := flag.String("redis-addr", "localhost:6379", "Redis hostname:port")
 	redisPw := flag.String("redis-pw", "", "Redis password")
 	redisDb := flag.Int("redis-db", 0, "Redis database (0-15)")
+	cliMode := flag.Bool("cli", false, "Enable CLI mode instead of http server")
 	flag.Parse()
 
 	db, err := NewDB(*redisAddr, *redisPw, *redisDb)
@@ -21,7 +22,12 @@ func main() {
 
 	defer db.Close()
 
-	addr := "localhost:" + strconv.Itoa(*port)
-	srv := newServer(db, *hostname)
-	http.ListenAndServe(addr, srv)
+	if *cliMode {
+		cli := newCLI(db)
+		cli.Run()
+	} else {
+		addr := "localhost:" + strconv.Itoa(*port)
+		srv := newServer(db, *hostname)
+		http.ListenAndServe(addr, srv)
+	}
 }
