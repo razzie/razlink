1106ae59b68a71b705a2473622e0533c9a3701b2
Gábor Görzsöny <gabor@gorzsony.com>
2020-01-19 06:08:56 +0100

    New fancy background pattern

--- layout.go
+++ layout.go
@@ -14,12 +14,17 @@ var layout = `
 		<base href="{{.Base}}" target="_blank">
 		<link rel="icon" href="favicon.svg" type="image/svg+xml" />
 		<style>
+		body {
+			background-color: #ffffff;
+			background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23808080' fill-opacity='0.5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+		}
 		div.outer {
 			display: flex;
 			align-items: center;
 			justify-content: center;
 		}
 		div.inner {
+			background-color: #ffffff;
 			border: 1px solid black;
 			padding: 1rem;
 			display: inline-flex;
