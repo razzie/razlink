a7b52994827fbb91b1a7729b9f0cfc699172777b
Gábor Görzsöny <gabor@gorzsony.com>
2020-05-24 12:25:12 +0200

    Improved link formatting

--- layout.go
+++ layout.go
@@ -20,6 +20,15 @@ var layoutT = `
 			background-color: white;
 			background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23808080' fill-opacity='0.5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 		}
+		a {
+			color: black;
+			text-decoration: underline;
+			text-decoration-color: rgb(220, 53, 69);
+			-webkit-text-decoration-color: rgb(220, 53, 69);
+		}
+		a:hover {
+			color: dimgrey;
+		}
 		div.outer {
 			display: flex;
 			align-items: center;
